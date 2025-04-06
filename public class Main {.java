8060b2a441dbc1031d32504c0a8b4fa53061ba6b
Maciek <maciek@gmail.com>
2025-04-06 10:29:54 +0200

    dzien 2 hello

--- public class Main {.java
+++ public class Main {.java
@@ -15,5 +15,40 @@ public class Main {
             suma += liczby[i];
         }
         System.out.println("Suma elementów tablicy: " + suma);
+
+        // Znajdowanie największego i najmniejszego elementu w tablicy
+        int najwieksza = liczby[0];
+        int najmniejsza = liczby[0];
+
+        for (int i = 1; i < liczby.length; i++) {
+            if (liczby[i] > najwieksza) {
+                najwieksza = liczby[i];
+            }
+            if (liczby[i] < najmniejsza) {
+                najmniejsza = liczby[i];
+            }
+        }
+        System.out.println("Największy element tablicy: " + najwieksza);
+        System.out.println("Najmniejszy element tablicy: " + najmniejsza);
+
+        // Odwrócenie tablicy
+        System.out.println("Tablica po odwróceniu:");
+        for (int i = liczby.length - 1; i >= 0; i--) {
+            System.out.println(liczby[i]);
+        }
+
+        // Wyszukiwanie liczby w tablicy
+        int szukanaLiczba = 3;
+        boolean znaleziono = false;
+        for (int i = 0; i < liczby.length; i++) {
+            if (liczby[i] == szukanaLiczba) {
+                znaleziono = true;
+                System.out.println("Liczba " + szukanaLiczba + " znajduje się w tablicy.");
+                break;
+            }
+        }
+        if (!znaleziono) {
+            System.out.println("Liczba " + szukanaLiczba + " nie znajduje się w tablicy.");
+        }
     }
 }
