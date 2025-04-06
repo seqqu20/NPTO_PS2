b2b2aa12b6c9f4b6711b4c92cfd2a0c3b39076db
Maciek <maciek@gmail.com>
2025-04-06 10:34:54 +0200

    usuniecie komentarzy

--- public class Main {.java
+++ public class Main {.java
@@ -1,22 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        // Tworzymy tablicę typu int z 5 elementami
         int[] liczby = {1, 2, 3, 4, 5};
 
-        // Wyświetlamy wszystkie elementy tablicy
         System.out.println("Elementy tablicy:");
         for (int i = 0; i < liczby.length; i++) {
             System.out.println("Element " + (i + 1) + ": " + liczby[i]);
         }
 
-        // Obliczamy sumę wszystkich elementów tablicy
         int suma = 0;
         for (int i = 0; i < liczby.length; i++) {
             suma += liczby[i];
         }
         System.out.println("Suma elementów tablicy: " + suma);
 
-        // Znajdowanie największego i najmniejszego elementu w tablicy
         int najwieksza = liczby[0];
         int najmniejsza = liczby[0];
 
@@ -31,13 +27,11 @@ public class Main {
         System.out.println("Największy element tablicy: " + najwieksza);
         System.out.println("Najmniejszy element tablicy: " + najmniejsza);
 
-        // Odwrócenie tablicy
         System.out.println("Tablica po odwróceniu:");
         for (int i = liczby.length - 1; i >= 0; i--) {
             System.out.println(liczby[i]);
         }
 
-        // Wyszukiwanie liczby w tablicy
         int szukanaLiczba = 3;
         boolean znaleziono = false;
         for (int i = 0; i < liczby.length; i++) {
